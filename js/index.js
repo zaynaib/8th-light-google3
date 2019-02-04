3d436724a7b36e073dd5e14969be34bbdf5d17d0
Zaynaib Giwa <zaynaibg@gmail.com>
2019-02-04 01:07:13 -0600

    deleted commented code and add classes col and row

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ $(document).ready(function (){
             
             
             // Creating new Div and assigning to a newDiv variable and giving it a class of booksDiv   
-            let newDiv = $("<div>").addClass("booksDiv");
+            let newDiv = $("<div>").addClass("booksDiv").addClass("col-4");
     
         // New ImageDiv with a Class of BookCover and assigning a src of the bookCover variable
        let imageDiv = $("<img>").addClass("bookCover").attr("src", bookCover);
@@ -81,89 +81,7 @@ $(document).ready(function (){
       
       
       
-      /*
-
-       if ( $("#bookName").val() === ""){
-           alert("Please Enter A Book Name to Search")
-       } else {
-
-        let bookName = $("#bookName").val();
-        $("#bookName").empty();
-        $("#resultsDiv").empty();
-        //searchKeywords.push(bookName);
-        
-        //debug code 
-        console.log(bookName);
-        console.log(searchKeywords);
-        
-
-            
-    let queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + bookName;
-
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).done(function(response) {
-
-           console.log(response);
-
-                for (var i = 0; i < 10; i++) {
-                    // incase Book thumbnail isnt available, use BooGle Image
-                    if (bookCover = undefined) {
-                        bookCover = "../assets/images/boogleImage.png"; }
-                        
-                        if (categories === undefined || 
-                            pageCount === undefined || 
-                            description === undefined || 
-                            publisher === undefined || 
-                            publishedDate === undefined ) {
-    
-                            categories === "Not Available" || 
-                            pageCount === "Not Available" ||
-                            description === "Not Available" ||
-                            publisher === "Not Available" ||
-                            publishedDate === "Not Available"
-                        };
-                // saving each data we need from the response to a variable
-                    var title = response.items[i].volumeInfo.title;
-                    var author = response.items[i].volumeInfo.authors;
-                    var categories = response.items[i].volumeInfo.categories;
-                    var description = response.items[i].volumeInfo.description;
-                    var pageCount = response.items[i].volumeInfo.pageCount;
-                    var publishedDate = response.items[i].volumeInfo.publishedDate;
-                    var publisher = response.items[i].volumeInfo.publisher;
-                    var bookCover = response.items[i].volumeInfo.imageLinks.thumbnail;
-
-// Creating new Div and assigning to a newDiv variable and giving it a class of booksDiv   
-                var newDiv = $("<div>").addClass("booksDiv");
-    
-        // New ImageDiv with a Class of BookCover and assigning a src of the bookCover variable
-                var imageDiv = $("<img>").addClass("bookCover").attr("src", bookCover);
-
-        // new Div with class of bookInfo and insert all the bookinfo in the Div
-                var bookInfoDiv = $("<div>").addClass("bookInfo").html(
-                        "Author: " + author + 
-                        "<p> Title: " + title + "</p>" + 
-                        "<p> Category: " + categories + "</p>" + 
-                        "<p> Number of Pages: " + pageCount + "</p>" + 
-                        "<p> Published on: " + publishedDate + "</p>" + 
-                        "<p> Published by: " + publisher + "</p>" + 
-                        "<p> Description: " + description + "</p>"
-                    );
-                
-            // append imageDiv & BookInfo to newDiv
-                newDiv.append(imageDiv);
-                newDiv.append(bookInfoDiv)
-            // append newDiv to DOM
-                $("#resultsDiv").append(newDiv);
-                
-            }
-                
-               
-            });
-     } 
-    
-    */
+     
     })
 
 
