10a9cdf58a16c9c450cedb309f96432d41ced1ea
Zaynaib Giwa <zaynaibg@gmail.com>
2019-02-12 19:39:36 -0600

    make your own jquery

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-var resultsDiv = $("#resultsDiv");
+var $ = (s, d = document) => Array.from(d.querySelectorAll(s));
+$.one = (s, d = document) => d.querySelector(s);
+$.create = tag => document.createElement(tag);
+
+var resultsDiv = $.one("#resultsDiv");
 
 //when the user clicks on search
-$(document).on("click", "#searchBook", async function (e) {
+$.one("#searchBook").addEventListener("click", async function (e) {
   e.preventDefault();
 
   let bookName = $("#bookName").val();
-  resultsDiv.empty();
+  resultsDiv.innerHTML = "";
 
   let queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + bookName;
 
@@ -26,23 +30,29 @@ $(document).on("click", "#searchBook", async function (e) {
     if (!title) continue;
     let bookCover = bookItem.volumeInfo.imageLinks.thumbnail;
 
-    let bookDiv = $("<div>").addClass("booksDiv").addClass("col-4");
+    let bookDiv = $.create("div")
+    bookDiv.classList.add("booksDiv");
+    bookDiv.classList.add("col-4");
 
     // New ImageDiv with a Class of BookCover and assigning a src of the bookCover variable
-    let imageDiv = $("<img>").addClass("bookCover").attr("src", bookCover);
+    let imageDiv = $.one("img");
+    imageDiv.classList.add("bookCover");
+    imageDiv.setAttribute("src", bookCover);
 
-    let bookInfoDiv = $("<div>").addClass("bookInfo").html(`
+    let bookInfoDiv = $.create("div")
+    bookInfoDiv.classList.add("bookInfo")
+    bookInfoDiv.innerHTML = `
       <p> Title: ${title}  </p> 
       <p> Author: ${authors}</p>
       <p> Published by: ${publisher}</p>
-      <a href= "${previewLink}">Find out more about ${title}</a>`);
+      <a href= "${previewLink}">Find out more about ${title}</a>`;
 
-    bookDiv.append(imageDiv);
-    bookDiv.append(bookInfoDiv);
+    bookDiv.appendChild(imageDiv);
+    bookDiv.appendChild(bookInfoDiv);
 
 
     // append newDiv to DOM
-    resultsDiv.append(bookDiv);
+    resultsDiv.appendChild(bookDiv);
 
   }
 
